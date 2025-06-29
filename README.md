fa1708e8806fdb1536cb04f4e1b2de6e3a15ac7c
unknown <ryankahinglee@gmail.com>
2025-06-29 16:45:34 +1000

    added Material UI libary and testrun for page hosting

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# honeCollegeV1
-honeCollegeV1 Website (temp)
+# Hone College Website
+
+This is a React application built with Create React App (CRA), styled using Material UI (MUI), and deployed to GitHub Pages.
+
+git clone https://github.com/ryankahinglee/honeCollegeV1.git
+cd honeCollegeV1
+npm install
+npm start
--- hone_college_v1/src/App.tsx
+++ hone_college_v1/src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Sample pages
+function Home() {
+  return <h2>Home Page</h2>;
+}
+function About() {
+  return <h2>About Page</h2>;
+}
+function Contact() {
+  return <h2>Contact Page</h2>;
 }
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Hone College
+          </Typography>
+          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button color="inherit" component={Link} to="/about">About</Button>
+          <Button color="inherit" component={Link} to="/contact">Contact</Button>
+        </Toolbar>
+      </AppBar>
+
+      <Container sx={{ mt: 4 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Container>
+    </Router>
+  );
+}
